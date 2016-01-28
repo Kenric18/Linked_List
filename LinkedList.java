7a9a0a253375de48b37da725b1d0929c010c9304
kenricd18@gmail.com <kenricd18@gmail.com>
2016-01-28 10:57:43 +0000

    First commit

---
+++ LinkedList.java
@@ -0,0 +1,26 @@
+class LinkedList {
+
+  public LinkedList () {
+  }
+
+  public boolean isEmpty () {
+    return true;
+  }
+
+  public void insert (int d) {
+    return;
+  }
+
+  public int delete () {
+    return 0;
+  }
+
+  public boolean has (int d) {
+    return false;
+  }
+
+  public void print () {
+    return;
+  }
+
+}
---
+++ ListTest.java
@@ -0,0 +1,29 @@
+class ListTest {
+
+  public static void main(String[] args) {
+
+    LinkedList list = new LinkedList();
+
+    list.insert (1);
+    list.insert (3);
+    list.insert (6);
+    list.insert (5);
+
+    list.print ();
+
+    for (int l = 0; l < 6; ++l) {
+      if (!list.has(l)) {
+        System.out.println (l + " is not in the list");
+      }
+    }
+
+    while (!list.isEmpty()) {
+      int d = list.delete();
+      System.out.println("deleted: " + d);
+    }
+
+    list.print ();
+
+  }
+
+}
