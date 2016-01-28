abc02ad3a4874c733ec174b50ce14f7f4ddf48e0
Kenric18 <kenricd18@gmail.com>
2016-01-28 12:22:33 +0000

    Node class created

---
+++ Node.java
@@ -0,0 +1,15 @@
+public class Node {
+
+	private int nodeValue;
+
+	public Node(int nodeValue) {
+		this.nodeValue = nodeValue;
+	}
+
+	public getValue() {
+		return nodeValue;
+	}
+
+	public setValue(int value)
+		nodeValue = value;
+}
