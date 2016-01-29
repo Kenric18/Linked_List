121cdec916cdd0c45a8bd10df0373303fcd745f6
kenricd18@gmail.com <kenricd18@gmail.com>
2016-01-29 10:43:12 +0000

    added Pointers

--- Node.java
+++ Node.java
@@ -1,15 +1,28 @@
 public class Node {
 
 	private int nodeValue;
+	private Node pointer;
 
 	public Node(int nodeValue) {
 		this.nodeValue = nodeValue;
 	}
 
-	public getValue() {
+	public Node(int nodeValue, Node pointer) {
+		this.nodeValue = nodeValue;
+		this.pointer = pointer;
+	}
+
+	public int getValue() {
 		return nodeValue;
 	}
 
-	public setValue(int value)
+	public void setValue(int value) {
 		nodeValue = value;
+	}
+
+	public Node getPointer() {
+		return pointer;
+	}
+
+
 }
